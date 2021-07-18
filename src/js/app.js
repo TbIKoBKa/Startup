261e658a76009152f9b29a10f0d9eed1da91b3b2
Eugene Mormul <zhenya.mormul@gmail.com>
2021-07-18 22:36:46 +0300

    Fixed header/Layout fixes

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,18 @@
 let offcanvas = document.querySelector('.offcanvas');
+let header = document.querySelector('.header');
 let touchForm = document.forms.touch;
+
+function checkScroll(){
+    (window.pageYOffset > header.style.height + 200) ? header.classList.add('fixed') : header.classList.remove('fixed');
+}
+
+(function(){
+    checkScroll()
+}())
+
+window.addEventListener('scroll', () => {
+    checkScroll()
+})
 
 offcanvas.addEventListener('hidden.bs.offcanvas', (event) => {
     event.stopImmediatePropagation();
